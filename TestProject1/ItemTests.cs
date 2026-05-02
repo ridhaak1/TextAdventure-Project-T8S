31dd836f86454288f56386ddc5a32b0676251986
Furkan <furkan.ozaydin@student.ap.be>
2026-05-02 15:51:21 +0200

    initial tests added

---
+++ TestProject1/ItemTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextAdventure;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class ItemTests
+    {
+        [TestMethod]
+        public void Item_SetsNameAndDescription()
+        {
+            var item = new Item("Sleutel", "Een gouden sleutel.");
+
+            Assert.AreEqual("Sleutel", item.Name);
+            Assert.AreEqual("Een gouden sleutel.", item.Description);
+        }
+
+        [TestMethod]
+        public void Item_RecordEquality_SameValues_AreEqual()
+        {
+            var a = new Item("Zwaard", "Een scherp zwaard.");
+            var b = new Item("Zwaard", "Een scherp zwaard.");
+
+            Assert.AreEqual(a, b);
+        }
+
+        [TestMethod]
+        public void Item_RecordEquality_DifferentValues_AreNotEqual()
+        {
+            var a = new Item("Zwaard", "Een scherp zwaard.");
+            var b = new Item("Sleutel", "Een gouden sleutel.");
+
+            Assert.AreNotEqual(a, b);
+        }
+    }
+
+}
